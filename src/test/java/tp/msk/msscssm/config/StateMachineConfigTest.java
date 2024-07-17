22b31d03814f83061ad752bafa7dc52ee5216ecd
Sane Generous <karim2k11@gmail.com>
2024-07-17 15:35:24 +0100

    StateMachineConfig created along with unit test StateMachineConfigTest

---
+++ src/test/java/tp/msk/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,38 @@
+package tp.msk.msscssm.config;
+
+import org.apache.naming.factory.SendMailFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import tp.msk.msscssm.domain.PaymentEvent;
+import tp.msk.msscssm.domain.PaymentState;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void nameNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+
+        sm.start();
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(sm.getState().toString());
+    }
+}
